5b57b58a56985db21055c705ef549ecd86a4e548
Stealth22 <stealth22pd@gmail.com>
2018-04-20 16:33:43 -0700

    Update Common dll

--- Traffic Control/Common/Constants.cs
+++ Traffic Control/Common/Constants.cs
@@ -12,7 +12,7 @@ namespace Stealth.Plugins.TrafficControl.Common
         internal const int LCPDFRDownloadID = 7953;
         internal const string DLLPath = @"Plugins\LSPDFR\Traffic Control.dll";
 
-        internal const string ReqCommonVersion = "2.0.6684.27674";
+        internal const string ReqCommonVersion = "2.0.6684.29778";
         internal const string ReqRPHVersion = "0.62.1216.14731";
         internal const string ReqLSPDFRVersion = "0.3.38.5436";
         internal const string ReqRNUIVersion = "1.6.3.0";
